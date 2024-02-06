d634314e2bb3da9177571313a5e346d6e328bee2
Nicole Sanchez <nicole.sanchez11@outlook.com>
2024-02-06 15:12:37 +0100

    added moviesgroup UI models

---
+++ MoviesGroup.UI.Models/Link/LinkOption.cs
@@ -0,0 +1,8 @@
+﻿//namespace eShop.UI.Models.Link;
+
+//public class LinkOption
+//{
+//    public int id { get; set; }
+//    public string Name { get; set; }
+//    public bool IsSelected { get; set; }
+//}
