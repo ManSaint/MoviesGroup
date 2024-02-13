e3ac95ccda5522e42155c13988832d9d849234dd
Hembrent <143487953+Hembrent@users.noreply.github.com>
2024-02-13 13:20:51 +0100

    Changes made to UI.

--- MoviesGroup.UI.Http/Clients/GenreHttpClient.cs
+++ MoviesGroup.UI.Http/Clients/GenreHttpClient.cs
@@ -1,4 +1,7 @@
-﻿namespace MoviesGroup.UI.Http;
+﻿using MoviesGroup.API.DTO;
+using System.Text.Json;
+
+namespace MoviesGroup.UI.Http;
 public class GenreHttpClient
 {
     private readonly HttpClient _httpClient;
@@ -8,5 +11,23 @@ public class GenreHttpClient
     {
         _httpClient = httpClient;
         _httpClient.BaseAddress = new Uri($"{_baseAddress}categorys");
+    }
+
+    public async Task<List<GenreGetDTO>> GetGenresAsync()
+    {
+        try
+        {
+            using HttpResponseMessage response = await _httpClient.GetAsync("");
+            response.EnsureSuccessStatusCode();
+
+            var result = JsonSerializer.Deserialize<List<GenreGetDTO>>(await response.Content.ReadAsStreamAsync(),
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            return result ?? [];
+        }
+        catch (Exception ex)
+        {
+            return [];
+        }
     }
 }
--- MoviesGroup.UI/Services/UIService.cs
+++ MoviesGroup.UI/Services/UIService.cs
@@ -1,18 +1,41 @@
-﻿namespace MoviesGroup.UI.Services;
-public class UIService(GenreHttpClient genreHttp)
+﻿namespace eShop.UI.Services;
+
+public class UIService(GenreHttpClient genreHttp,
+    GenreHttpClient productHttp, IMapper mapper)
 {
-    List<GenreGetDTO> Genres { get; set; } = [];
-    public List<LinkGroup> GenreLinkGroups { get; private set; } =
+    List<GenreGetDTO> Categories { get; set; } = [];
+    public List<GenreGetDTO> Products { get; private set; } = [];
+    public List<LinkGroup> CaregoryLinkGroups { get; private set; } =
     [
         new LinkGroup
         {
-            Name = "Genres",
-            LinkOptions = new(){
+            Name = "Categories"
+            /*,LinkOptions = new(){
                 new LinkOption { Id = 1, Name = "Women", IsSelected = true },
                 new LinkOption { Id = 2, Name = "Men", IsSelected = false },
                 new LinkOption { Id = 3, Name = "Children", IsSelected = false }
-            }
+            }*/
         }
     ];
-    public int CurrentGenreId { get; set; }
+    public int CurrentCategoryId { get; set; }
+
+    public async Task GetLinkGroup()
+    {
+        Categories = await genreHttp.GetGenresAsync();
+        CaregoryLinkGroups[0].LinkOptions = mapper.Map<List<LinkOption>>(Categories);
+        var linkOption = CaregoryLinkGroups[0].LinkOptions.FirstOrDefault();
+        linkOption!.IsSelected = true;
+    }
+
+    public async Task OnCategoryLinkClick(int id)
+    {
+        CurrentCategoryId = id;
+        await GetProductsAsync();
+        CaregoryLinkGroups[0].LinkOptions.ForEach(l => l.IsSelected = false);
+        CaregoryLinkGroups[0].LinkOptions.Single(l => l.Id.Equals(CurrentCategoryId)).IsSelected = true;
+    }
+
+    public async Task GetProductsAsync() =>
+        Products = await productHttp.GetMoviesAsync(CurrentCategoryId);
+
 }
