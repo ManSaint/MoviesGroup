9dca89af32a3ef20ec926520fd7c5f5284cfc9db
Lupin0101 <basher461@icloud.com>
2024-02-20 14:43:13 +0100

    added SessionStroge , LocalStorage , IStorageServices

---
+++ MoviesGroup.UI.Storage/Services/SessionStorage.cs
@@ -0,0 +1,17 @@
+﻿using Blazored.SessionStorage;
+using MoviesGroup.UI.Storage.Services;
+
+namespace MoviesGroup.UI.Storage.Services;
+
+public class SessionStorage(ISessionStorageService sessionStorage)
+    : IStorageService
+{
+    public async Task<T?> GetAsync<T>(string key) =>
+        await sessionStorage.GetItemAsync<T>(key);
+
+    public async Task RemoveAsync(string key) =>
+        await sessionStorage.RemoveItemAsync(key);
+
+    public async Task SetAsync<T>(string key, T value) =>
+        await sessionStorage.SetItemAsync(key, value);
+}
